c797e699e31a09d85fa0da7cdfc57dd026379651
nenadtod <nenadtod@live.com>
2017-06-05 03:04:46 +0200

    Added configuration for reference loop handling, and enabled CORS.

--- BookingApp/BookingApp/App_Start/WebApiConfig.cs
+++ BookingApp/BookingApp/App_Start/WebApiConfig.cs
@@ -11,12 +11,15 @@ namespace BookingApp
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
-        {
+        {	
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
+			
+			var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+			
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -25,6 +28,8 @@ namespace BookingApp
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+			
+			config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
         }
     }
 }
